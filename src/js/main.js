cdd9900b6a914ed533c455995f1677b469c15ba7
Valeriia <lera_kanavchenko@ukr.net>
2024-08-20 16:18:27 +0300

    Add img gallery and some libraries

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+import iziToast from "izitoast";
+import SimpleLightbox from "simplelightbox";
+
+import { fetchPhotos } from './pixabay-api';
+import { renderGalleryItem } from './render-functions.js';
+
+const formEl = document.querySelector('.form');
+const inputEl = document.querySelector('.form-input');
+const gallery = document.querySelector('.gallery-list');
+export const loader = document.querySelector('.loader');
+
+
+formEl.addEventListener('submit', submitForm);
+
+const lightbox = new SimpleLightbox('.gallery-item a', {captionsData: 'alt'});
+
+function submitForm(event) {
+    event.preventDefault();
+    gallery.innerHTML = '';
+    const searchedInfo = inputEl.value;
+    if (searchedInfo === '') {
+        return
+    }
+
+    fetchPhotos(searchedInfo)
+        .then(data => {
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                    messageColor: 'white',
+                    messageSize: '16',
+                    color: 'red',
+                    iconColor: 'white'
+                });
+            };
+
+            const createGallery = data.hits.map(imgDetail => renderGalleryItem(imgDetail)).join('');
+        
+            loader.classList.add('is-hidden');
+            gallery.insertAdjacentHTML('afterbegin', createGallery);
+            lightbox.refresh();
+            formEl.reset();
+        })
+        .catch (error => console.log(error));
+}
+
+
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import { loader } from './main.js';
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function fetchPhotos(value) {
+    loader.classList.remove('is-hidden');
+    const URLParams = new URLSearchParams({
+    key: '45515322-6acc0c0fe7102921e2f085c71',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true
+    })
+    
+    return fetch(`${BASE_URL}?${URLParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            return response.json();
+        })
+}
+
